343a07548e770812384fa7582ceca1b75e82c9fa
Conor-Hinchee <conorphinchee@gmail.com>
2024-12-21 11:48:11 -0500

    updating consent animation

--- src/Home/js/components/consentBanner.js
+++ src/Home/js/components/consentBanner.js
@@ -16,27 +16,52 @@ const showConsentBannerPeak = () => {
   const banner = document.getElementById("consentBanner");
   banner.style.transitionDuration = "1.3s";
   banner.style.opacity = "1";
+  banner.addEventListener("transitionend", () => {
+    const event = new Event("consentBannerPeakComplete");
+    banner.dispatchEvent(event);
+  }, { once: true });
 };
 
 const showConsentBannerBar = () => {
   const banner = document.getElementById("consentBanner");
   banner.style.transitionDuration = "1.3s";
   banner.style.width = "100%";
+  banner.addEventListener("transitionend", () => {
+    const event = new Event("consentBannerBarComplete");
+    banner.dispatchEvent(event);
+  }, { once: true });
 };
 
 const showConsentBannerFull = () => {
-  console.log("showConsentBannerFull");
   window.removeEventListener("scroll", scrollListener);
 
   const banner = document.getElementById("consentBanner");
+  const consentTitle = document.getElementById("consentTitle");
+  consentTitle.style.transitionDelay = "1s";
+  consentTitle.style.transitionDuration = "2.5s";
+  consentTitle.style.width = "50%";
+
   banner.style.transitionDuration = ".5s";
   banner.style.height = "250px";
   banner.style.width = "100%";
+  banner.addEventListener("transitionend", () => {
+    const event = new Event("consentBannerFullComplete");
+    banner.dispatchEvent(event);
+  }, { once: true });
 
+};
+
+const showConsentContent = () => {
   const consentContent = document.getElementById("consentContent");
-  consentContent.style.transitionDuration = "1.3s";
-  consentContent.style.display = "block";
+  // const consentTitle = document.getElementById("consentTitle");
+  consentContent.classList.remove("hidden");
+
+  consentContent.style.transitionDuration = "1.5s";
+  // consentTitle.style.transitionDuration = "1.5s";
+  // consentTitle.style.width = "50%";
+  consentContent.style.width = "50%";
   consentContent.style.opacity = "1";
+
 };
 
 const scrollListener = () => {
@@ -46,6 +71,19 @@ const scrollListener = () => {
 
   if (window.scrollY > 500) {
     showConsentBannerFull();
+    window.removeEventListener("scroll", scrollListener);
+  }
+};
+
+const eventConductorSteve = (event) => {
+  console.log("event", event, event.type);
+  if (event.type === "consentBannerPeakComplete") {
+    // placeholder for future animations.
+  }
+
+  if (event.type === "consentBannerFullComplete") {
+    // setTimeout(showConsentBannerFull, 2000);
+    showConsentContent();
   }
 };
 
@@ -91,6 +129,11 @@ const initConsentBanner = () => {
     localStorage.setItem("gtagConsent", JSON.stringify(CONSENT_DECLINED));
     banner.style.display = "none";
   });
+
+  banner.addEventListener("consentBannerPeakComplete", eventConductorSteve);
+  banner.addEventListener("consentBannerBarComplete", eventConductorSteve);
+  banner.addEventListener("consentBannerFullComplete", eventConductorSteve);
+
 };
 
 export default initConsentBanner;
