ef9b7a0ba5a04ee07fd8c3dce6156682884c2c1e
Conor-Hinchee <conorphinchee@gmail.com>
2025-04-01 00:00:00 +0000

    updating-about-me

--- src/Home/js/blocking.js
+++ src/Home/js/blocking.js
@@ -1,36 +1,56 @@
 // THIS IS A BLOCKING SCRIPT ANYTHING ADDED HERE WILL ADD TO THE PAGE LOAD TIME
-
 const useDarkMode = () => {
   document.documentElement.classList.add("dark");
-  localStorage.setItem("theme", "dark");
+  // localStorage.setItem("theme", "dark");
 };
 
 const useLightMode = () => {
   document.documentElement.classList.remove("dark");
-  localStorage.setItem("theme", "light");
+  // localStorage.setItem("theme", "light");
+};
+
+const getOSPreference = () => {
+  return window.matchMedia?.("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+};
+
+const watchOSTheme = () => {
+  window
+    .matchMedia?.("(prefers-color-scheme: dark)")
+    .addEventListener("change", (e) => {
+      if (!localStorage.getItem("theme")) {
+        e.matches ? useDarkMode() : useLightMode();
+      }
+    });
 };
 
 const initTheme = () => {
-  if (localStorage.getItem("theme") === "dark") {
-    useDarkMode();
-    return;
-  }
-
-  if (localStorage.getItem("theme") === "light") {
-    useLightMode();
-    return;
-  }
-
-  if (window.matchMedia && !localStorage.getItem("theme")) {
-    // FIRST TIME USER HAS AN OS PREFERENCE of Dark set to Dark
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  try {
+    if (localStorage.getItem("theme") === "dark") {
       useDarkMode();
       return;
     }
-  }
 
-  useLightMode();
+    if (localStorage.getItem("theme") === "light") {
+      useLightMode();
+      return;
+    }
+
+    if (window.matchMedia && !localStorage.getItem("theme")) {
+      const osPreference = getOSPreference();
+      if (osPreference === "dark") {
+        useDarkMode();
+        return;
+      }
+
+      useLightMode();
+    }
+  } catch (e) {
+    console.error("Error initializing theme:", e);
+    useLightMode();
+  }
 };
 
-// LIGHT/DARK MODE ARE HERE TO PREVENT FOUC
 initTheme();
+watchOSTheme();
